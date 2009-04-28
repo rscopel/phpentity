5faa83eb8aadb452999ba647767acff3538403d3
dvogels <dvogels@9d6754aa-efaa-11dd-a320-db7d8e7a638f>
2009-04-28 02:34:46 +0000

    updated jQuery from 1.3.1 to 1.3.2

--- trunk/app/includes/core/mainJquery.php
+++ trunk/app/includes/core/mainJquery.php
@@ -17,6 +17,6 @@
  * Path is from static site root, must bootstrap before loading this file.
  * Include only when needed.
  */
-define('DV_JQUERY_INCLUDE', DV_STATIC_WEB_ROOT.'resources/javascript/jquery/1.3.1/jquery-1.3.1.min.js');
+define('DV_JQUERY_INCLUDE', DV_STATIC_WEB_ROOT.'resources/javascript/jquery/1.3.2/jquery-1.3.2.min.js');
 
 ?>
